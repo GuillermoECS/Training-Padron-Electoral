69652b29a5bf9a59d872a66bbbecaa53092e438b
Felipe Castro <luisfelipecs97@gmail.com>
2018-12-18 14:30:10 -0600

    PostgreSQL Settings

--- padron/padron/settings.py
+++ padron/padron/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'padron.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DBNAME', 'padron'),
+        'USER': os.getenv('DBUSER', 'padron_user'),
+        'PASSWORD': os.getenv('DBPASSWORD', 'padron_user'),
+        'HOST': os.getenv('DBHOST', '127.0.0.1'),
+        'PORT': os.getenv('DBPORT', '5432'),
     }
 }
 
